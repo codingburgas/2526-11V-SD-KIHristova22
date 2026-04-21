dd1bc7c10e1e8e906dfe45b74487974c3b8a0699
KIHristova22 <KIHristova22@codingburgas.bg>
2026-04-21 09:58:41 +0300

    update the ui styling

--- BookStore/BookStore/obj/Debug/net10.0/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net10.0/BookStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb1a9700a9eb8bfeb791e6fefa77131f5352e01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dceafef04b6c68f119c6881827572c606223d281")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
