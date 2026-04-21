27067ef56ec5e3d324b1a49d1c880167912b6f94
KIHristova22 <KIHristova22@codingburgas.bg>
2026-04-21 10:12:19 +0300

    update the ui to all green

--- BookStore/BookStore/obj/Debug/net10.0/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net10.0/BookStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dceafef04b6c68f119c6881827572c606223d281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1bc7c10e1e8e906dfe45b74487974c3b8a0699")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
